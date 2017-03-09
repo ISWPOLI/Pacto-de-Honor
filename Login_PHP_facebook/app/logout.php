acf8936606fafc5b19f4381c5d8abc5f22223e4b
Juan Camilo Lancheros <jclancheros1@poligran.edu.co>
2017-03-08 20:58:32 -0500

    eliminiacion de archivos

--- Login_PHP_facebook/app/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['facebook']);
-header('Location: ../index.php');
--- Login_PHP_facebook/config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = array(
-	'app_id' => 'Aqui su Id de app facebook',
-	'app_secret' => 'Aqui su secret app facebook',
-	'scopes' => array('scope' => 'email,name')
-);
