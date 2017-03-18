7080ebd2064f7a93d0517461f3722f6c7014ab4a
Gabriel S. Álvarez <gabo05640@hotmail.com>
2017-03-18 11:44:59 -0500

    Se sube la versión base para el trabajo en grupo
    Archivos JS de historieta, avatar, personaje, mapa, creditos, batalla.
    Sprites de personajes, poderes, botones.
    Imagenes de mundos, botones.

---
+++ FRONTEND/js/historieta.js
@@ -0,0 +1,25 @@
+var histo;
+var button;
+var historieta = function(game){};
+historieta.prototype = {
+     preload: function(){
+          game.scale.pageAlignHorizontally = true;
+          game.scale.pageAlignVertically = true;
+
+          game.load.spritesheet('historieta', 'assets/historieta.png', 800, 600);
+          game.load.spritesheet('button', 'images/botonSaltar.png', 150, 40);
+     },
+     create: function(){
+         histo = game.add.sprite(0,0, 'historieta');
+         histo.animations.add('verH', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0.2, false);
+         histo.animations.play('verH');
+         button = game.add.button (game.world.width - 100, game.world.height - 100, 'button', this.verMapa, this, 2 ,1, 0);
+         button.anchor.setTo (0.5, 0.5);
+     },
+    verMapa: function(){
+        this.state.start("navegacion");
+    },
+    update: function(){
+       
+    }
+} 
