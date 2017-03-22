9bc493ab8d41e2f658c710de2065dce1ecb76f7d
jnrubiano <jnrubiano@gmail.com>
2017-03-22 15:53:57 -0500

    Test

--- BACKEND/PoliFigth/src/java/co/edu/poli/entidades/Jugador.java
+++ BACKEND/PoliFigth/src/java/co/edu/poli/entidades/Jugador.java
@@ -26,10 +26,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- *
- * @author Will
- */
+
 @Entity
 @Table(name = "jugador")
 @XmlRootElement
