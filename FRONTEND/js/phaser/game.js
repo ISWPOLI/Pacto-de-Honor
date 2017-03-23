4858e2cfdaba7ba5ac21f1bb45caec4d6184542e
david yepes buitrago <lion17-07@hotmail.com>
2017-03-22 21:38:15 -0500

    Realizamos cambios en las carpetas

--- FRONTEND/js/phaser/game.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game (800, 600, Phaser.CANVAS, 'game');
-    game.state.add("historieta", historieta);
-    game.state.add("navegacion", navegacion);
-    game.state.add("primer", primer);
-    game.state.add("seleccionpersonaje", seleccionpersonaje);    
-    game.state.add("seleccionavatar", seleccionavatar);    
-	game.state.start("seleccionavatar");
