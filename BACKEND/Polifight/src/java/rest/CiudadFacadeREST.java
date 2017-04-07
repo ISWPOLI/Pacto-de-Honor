523ba627614d824d088e74f6ba67540f84f95c55
jnrubiano <jrubiaob@everis.com>
2017-04-07 00:46:50 -0500

    Versión con pruebas, comentarios para el java doc y con los métodos funcionales de la entidad Ciudad

--- BACKEND/Polifight/src/java/rest/CiudadFacadeREST.java
+++ BACKEND/Polifight/src/java/rest/CiudadFacadeREST.java
@@ -11,20 +11,20 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 /**
- *
+ * Servicio REST para la entidad Ciudad
  * @author jrubiaob
  */
 @Stateless
-@Path("entitities.ciudad")
+@Path("ciudad")
 public class CiudadFacadeREST extends AbstractFacade<Ciudad> {
     @PersistenceContext(unitName = "PolifightPU")
     private EntityManager em;
@@ -32,51 +32,82 @@ public class CiudadFacadeREST extends AbstractFacade<Ciudad> {
     public CiudadFacadeREST() {
         super(Ciudad.class);
     }
-
+    
+    /**
+     * Crea un dato Ciudad
+     * Probado con el recurso "Crear"
+     * @param entity 
+     */
     @POST
     @Override
-    @Consumes({"application/xml", "application/json"})
+    @Path("create")
+    @Consumes({"application/json"})
     public void create(Ciudad entity) {
         super.create(entity);
     }
 
-    @PUT
-    @Path("{id}")
-    @Consumes({"application/xml", "application/json"})
-    public void edit(@PathParam("id") Integer id, Ciudad entity) {
-        super.edit(entity);
+    /**
+     * Edita un campo de acuerdo al id enviado
+     * Probado con el recurso "Editar"
+     * @param entity 
+     */
+    @POST
+    @Path("edit")
+    @Consumes({"application/json"})
+    public void edit(Ciudad entity) {
+        Ciudad ciudad = super.find(entity.getIdCiudad());
+        System.err.println("ID -> ");
+        ciudad.setNombreCiudad(entity.getNombreCiudad());
+        super.edit(ciudad);
     }
-
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
-    }
-
+    
+    /**
+     * Busca un dato de acuerdo al id enviado
+     * Probado con el recurso "Buscar"
+     * @param id
+     * @return 
+     */
     @GET
     @Path("find")
-    @Produces({"application/xml", "application/json"})
-    public Ciudad find(@PathParam("find") Integer id) {
+    @Produces({"application/json"})
+    public Ciudad find(@QueryParam("id") Integer id) {
         return super.find(id);
     }
 
+    /**
+     * Al consumir, arroja un json con todos los datos de la tabla
+     * Probado con el recurso "Ciudad"
+     * @return List<Ciudad>
+     */
     @GET
     @Override
-    @Produces({"application/xml", "application/json"})
+    @Produces({"application/json"})
     public List<Ciudad> findAll() {
         return super.findAll();
     }
 
+    /**
+     * Retorna de acuerdo alrango envidado, donde 0 es el primer dato
+     * Probado con el recurso "BuscaPorRango"
+     * @param from
+     * @param to
+     * @return 
+     */
     @GET
-    @Path("{from}/{to}")
-    @Produces({"application/xml", "application/json"})
-    public List<Ciudad> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+    @Path("findRange")
+    @Produces({"application/json"})
+    public List<Ciudad> findRange(@QueryParam("from") Integer from, @QueryParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
-
+    
+    /**
+     * Retorna el número de datos contenido en la tabla
+     * Probado con el recurso "NoDatos"
+     * @return 
+     */
     @GET
     @Path("count")
-    @Produces("text/plain")
+    @Produces({"application/json"})
     public String countREST() {
         return String.valueOf(super.count());
     }
