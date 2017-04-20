6990b23034080fe17fb4ede23ac4bb9d2529d939
DanteVDrako <ahsierra@poligran.edu.co>
2017-04-19 20:37:23 -0500

    Solucion conflictos

--- BACKEND/Polifight/src/java/rest/AbstractFacade.java
+++ BACKEND/Polifight/src/java/rest/AbstractFacade.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package com.poder.Restful.PH.service;
+package rest;
 
 import java.util.List;
 import javax.persistence.EntityManager;
--- BACKEND/Polifight/src/java/rest/PoderFacadeREST.java
+++ BACKEND/Polifight/src/java/rest/PoderFacadeREST.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package com.poder.Restful.PH.service;
+package rest;
 
 import com.poder.Restful.PH.Poder;
 import com.poder.Restful.PH.PoderPK;
