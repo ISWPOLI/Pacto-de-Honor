dc08e6eee8c331ace108364191c04dded99f2a74
nicolas96 <nicomako96@hotmail.com>
2017-04-20 23:14:14 -0500

    Responsive
    *Se añaden las funciones para mover la orientación del dispositivo
    
    *Se añade un estado con una barra de carga para configurar todos los
    ajustes de la pantalla
    
    *Se añaden funciones para saber desde que dispositivo se visualiza

--- FRONTEND/juego/js/phaser/game.js
+++ FRONTEND/juego/js/phaser/game.js
@@ -1,7 +1,8 @@
-var game = new Phaser.Game (800, 600, Phaser.CANVAS, 'game');
+var game = new Phaser.Game (800,600, Phaser.CANVAS, 'container');
     game.state.add("historieta", historieta);
     game.state.add("navegacion", navegacion);
     game.state.add("batalla", batalla);
+    game.state.add("boot",boot);
     game.state.add("creditos", creditos);
     game.state.add("compraPersonajes", compraPersonajes);
     game.state.add("rankings", rankings);
@@ -11,4 +12,4 @@ var game = new Phaser.Game (800, 600, Phaser.CANVAS, 'game');
     game.state.add("seleccionpersonaje", seleccionpersonaje);    
     game.state.add("seleccionavatar", seleccionavatar);
     game.state.add("fin", fin);    
-	game.state.start("seleccionavatar");
+	game.state.start("boot");
