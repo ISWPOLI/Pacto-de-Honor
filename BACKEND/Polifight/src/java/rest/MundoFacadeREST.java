41aea207b4f9eb90d890c3dd9dbb9a17014a7cfd
William T <wpradere@gmail.com>
2017-04-26 21:34:15 -0500

    Revert "Se documenta el servicio y se hacen pruevas basicas faltan soapui [issue #398]"
    This reverts commit 8b773ba1273c8963a0b13e876cc62dade84ce4fd.

--- BACKEND/Polifight/src/java/rest/MundoFacadeREST.java
+++ BACKEND/Polifight/src/java/rest/MundoFacadeREST.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
-import Entidades.Imagen;
 import com.sun.xml.bind.xmlschema.StrictWildcardPlug;
-import Entidades.Mundo;
+import entities.Mundo;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
@@ -15,7 +9,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -25,17 +18,16 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Servivio para la entidad Mundo 
+ *
  * @author Will
- * 
  */
 @Stateless
-@Path("mundo")
 public class MundoFacadeREST extends AbstractFacade<Mundo> {
-    @PersistenceContext(unitName = "PoliFightPU")
+
+    @PersistenceContext(unitName = "PolifightPU")
     private EntityManager em;
 
-     @Override
+    @Override
     protected EntityManager getEntityManager() {
         return em;
     }
@@ -43,40 +35,29 @@ public class MundoFacadeREST extends AbstractFacade<Mundo> {
     public MundoFacadeREST() {
         super(Mundo.class);
     }
-    
-    /**
-    *  Crea un dato mundo 
-    * @param entity Entidad Mundo
-    * @return String con informacion de satisfactorio o error en creacion
-    */
-    
+
     @POST
     @Path("create")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public String crearUsiario(Mundo entity) {
+    public String crearUsiario(Mundo entity, int idMundo, String nMundo) {
         String respuesta;
         try {
+            entity.setIdMundo(idMundo);
             em.persist(entity);
-            respuesta = "{'response':'Ok'}";
+            entity.setNombreMundo(nMundo);
+            respuesta = "Se crea con Exito  ";
         } catch (Exception e) {
             e.printStackTrace();
             StringWriter error = new StringWriter();
             e.printStackTrace(new PrintWriter(error));
-            respuesta =  "{'response':'KO', 'cause':'"+error.toString()+"'}";
+            respuesta = "no Responde a causa " + error.toString();
 
         }
         return respuesta;
 
     }
-    
-    
-    /**
-    *  Editar un dato mundo
-    * @param entity Entidad Mundo
-    * @return String con informacion de satisfactorio o error en creacion
-    */
-    
+
     @POST
     @Path("edit")
     @Consumes({MediaType.APPLICATION_JSON})
@@ -118,24 +99,16 @@ public class MundoFacadeREST extends AbstractFacade<Mundo> {
 
     }
     
-    /**
-    *  Busca  un dato por un parametro 
-    * @param Integer id parametro de busqueda 
-    * @return String si es satisfactorio genera la informacion si no error 
-    */
-    
-    @GET
+     @GET
     @Path("find")
     @Produces({MediaType.APPLICATION_JSON})
      
     public String find (@QueryParam("id")Integer id){
         Mundo mundo = em.find(Mundo.class, id);
-        Imagen imagen =em.find(Imagen.class, id);
-        
         String respuesta ;
         if (mundo != null){
             
-            respuesta = "{'idMundo':'"+mundo.getIdMundo() +"', 'nombreMundo':'"+mundo.getNombreMundo()+"', 'foto':'"+imagen.getFoto() +"'}";
+            respuesta = "{'idMundo':'"+mundo.getIdMundo() +"', 'nombreMundo':'"+mundo.getNombreMundo()  +"'}";
         }else{
             respuesta = "{'response':'KO','cause':'City not found'}";
        }
@@ -143,50 +116,23 @@ public class MundoFacadeREST extends AbstractFacade<Mundo> {
         return respuesta;
     }
     
-    /**
-    *  Lista todos los datos Mundo 
-    * @return String si es satisfactorio genera la informacion si no error 
-    */
-    
     @GET
     @Path("listMundo")
     @Produces({MediaType.APPLICATION_JSON})
     public String listarMundo(){
         
        String resultado = "[";
-       //Query query = em.createNamedQuery("SELECT h FROM Mundo h");
-       //
-       String query1 = "SELECT p FROM Mundo p"; 
-       String query2 = "SELECT s FROM Imagen s";
-        TypedQuery<Mundo> consultaMundo=em.createQuery(query1,Mundo.class);
-        List <Mundo> formundos = consultaMundo.getResultList();
-        
-        TypedQuery<Imagen> consultaImagen=em.createQuery(query2,Imagen.class);
-         List <Imagen> forimagen = consultaImagen.getResultList();
-        
-        
-        try {
-                       
-                   
-            for (int i =0; i < formundos.size();i++) {
+       Query query = em.createNamedQuery("SELECT h FROM Mundo h");
+       List <Mundo> formundos = query.getResultList();
+        for (int i =0; i < formundos.size();i++) {
             resultado += "{";
             if (i == formundos.size()-1 ){
                 
-                resultado += "'idMundo':'"+formundos.get(i).getIdMundo()+"', 'nombreMundo':'"+formundos.get(i).getNombreMundo()+"', 'foto':'"+ forimagen.get(i).getFoto() ;
+                resultado += "'idMundo':'"+formundos.get(i).getIdMundo()+"', 'nombreMundo':'"+formundos.get(i).getNombreMundo();
             }else{
-                resultado += "'idMundo':'"+formundos.get(i).getIdMundo()+"', 'nombreMundo':'"+formundos.get(i).getNombreMundo()+"', 'foto':'"+ forimagen.get(i).getFoto();
+                resultado += "'idMundo':'"+formundos.get(i).getIdMundo()+"', 'nombreMundo':'"+formundos.get(i).getNombreMundo();
             }
         }
-            
-        } catch (Exception e) {
-            
-            e.printStackTrace();
-       e.printStackTrace();
-            StringWriter error = new StringWriter();
-            e.printStackTrace(new PrintWriter(error));
-            resultado =  "{'response':'KO', 'cause':'"+error.toString()+"'}";
-        }
-        
        
        return resultado;
     }
