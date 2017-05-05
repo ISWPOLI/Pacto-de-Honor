76b87b124d43bcfc7108c49bfe9b13e3994d0c1d
CristanGarzonBernal <cristiangarzonbernal@gmail.com>
2017-05-05 00:33:22 -0500

    #473 Json e implementacion
    Se hace commt del Json de PersonajeEstado y se sube la implementacion en
    el codigo de javaScritp.

---
+++ FRONTEND/juego/js/validacionDeDesbloqueo/personajeEstado.js
@@ -0,0 +1,40 @@
+var personajesDesbloq = [{
+  "idP": {
+    "idpersonaje": "1",
+    "nombre":"Andres Gallo",
+    "estado ": "0"
+  },
+
+  "idP2": {
+    "idpersonaje ": "2",
+    "estado": "1"
+  },
+  "idP3": {
+    "idpersonaje ": "3",
+    "estado": "0"
+  },
+  "idP4": {
+    "idpersonaje ": "4",
+    "estado": "0"
+  },
+  "idP5": {
+    "idpersonaje ": "5",
+    "estado": "1"
+  },
+  "idP6": {
+    "idpersonaje ": "6",
+    "estado": "0"
+  },
+  "idP7": {
+    "idpersonaje ": "7",
+    "estado": "0"
+  },
+  "idP8": {
+    "idpersonaje ": "8",
+    "estado": "0"
+  },
+  "idP9": {
+    "idpersonaje ": "9",
+    "estado": "1"
+  }
+}]
--- FRONTEND/juego/js/validacionDeDesbloqueo/validacion.js
+++ FRONTEND/juego/js/validacionDeDesbloqueo/validacion.js
@@ -1,40 +1,54 @@
-window.onload = function() {
-    game = new Phaser.Game(800, 600, Phaser.AUTO, "");
-    game.state.add("regalado", regalado);
-    game.state.start("regalado");
-}
+
 var linea = [];
 var indice = 0;
 var indiceDelineaa = 0;
 var demoraEspacio = 100;
 var demoralineaa = 300;
-var isObteined;
+
+var idpj = "idP";
+var idpj2 = "idP2";
+var idpj3 = "idP3";
+var idpj4 = "idP4";
+var idpj5 = "idP5";
+var idpj6 = "idP6";
+var idpj7 = "idP7";
+var idpj8 = "idP8";
+var idpj9 = "idP9";
+
+var perDesbloqueado = personajesDesbloq[idpj].estado;
+var perDesbloqueado2 = personajesDesbloq[idpj2].estado;
+var perDesbloqueado3 = personajesDesbloq[idpj3].estado;
+var perDesbloqueado4 = personajesDesbloq[idpj4].estado;
+var perDesbloqueado5 = personajesDesbloq[idpj5].estado;
+var perDesbloqueado6 = personajesDesbloq[idpj6].estado;
+var perDesbloqueado7 = personajesDesbloq[idpj7].estado;
+var perDesbloqueado8 = personajesDesbloq[idpj8].estado;
+var perDesbloqueado9 = personajesDesbloq[idpj9].estado;
+
+
 var nombrePersonaje;
 var contenido = ["Felicidades has desbloqueado a  "];
 
-var regalado = function(game){};
-regalado.prototype = {
+var validacion  = function(game){};
+validacion.prototype = {
     preload: function(){
                 
     },
     create: function(){ 
 
-    	if(isObteined==true){
+    	if(perDesbloqueado=="1"){
 
 game.start.state("navegacion.js");
 
 }else{
 
-	$dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $usuario, $contraseña);
-$sql = ("SELECT * FROM  events WHERE Id in (SELECT MAX(Id) FROM events BY Personaje)");
-
 		    game.stage.backgroundColor = '#124184';
-text = game.add.text(32, 200, '', { font: "40px Arial", fill: "#FFFF00" });
+text = game.add.text(32, 200, personajesDesbloq[idpj].nombre, { font: "40px Arial", fill: "#FFFF00" });
 
-var a;
-a.setText(text + Personaje);
+/*var a;
+a.setText(text + personajesDesbloq[idpj].nombre);
 
-
+*/
  this.nextlinea();}
 	},nextlinea: function(){
 
@@ -47,7 +61,11 @@ a.setText(text + Personaje);
     game.time.events.repeat(demoraEspacio, linea.length, this.nextPalabra,this);
     indiceDelineaa++;
 
-	}, nextPalabra:function(){
+	}
+
+
+
+  , nextPalabra:function(){
 
    text.text = text.text.concat(linea[indice] + " ");
    indice++;
@@ -57,6 +75,6 @@ a.setText(text + Personaje);
        text.text = text.text.concat("\n");
        game.time.events.add(demoralineaa, this.nextlinea,this);
 
-	}
+  }
 }}
 	
