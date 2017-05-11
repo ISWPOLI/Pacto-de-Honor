eddc9a8d01e4b344c5bf9165312be1e6d6f585b7
jnrubiano <jrubiaob@everis.com>
2017-05-10 23:12:19 -0500

    Se arregla la fecha ya que estaba actualizando el token con fecha estática

---
+++ BACKEND/Polifight/build/generated-sources/ap-source-output/entities/Categoriaimagen_.java
@@ -0,0 +1,14 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-10T21:31:15")
+@StaticMetamodel(Categoriaimagen.class)
+public class Categoriaimagen_ { 
+
+    public static volatile SingularAttribute<Categoriaimagen, Integer> idcategoriaImagen;
+    public static volatile SingularAttribute<Categoriaimagen, String> descCategoriaImagen;
+
+}
