04cdf780c7916b828773b50bf2e6ef44b9503738
jnrubiano <jrubiaob@everis.com>
2017-05-13 10:18:53 -0500

    Actualización del proyecto Backend

--- BACKEND/BASES/ScriptsModificaciones/3.TablaLogros.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `logros` ( `id_logro` int(11) NOT NULL, `nombre` varchar(50) NOT NULL,
- `descripcion` varchar(200) NOT NULL, `esModenas` tinyint(1) NOT NULL, `esRanking` tinyint(1) NOT NULL,
- `esTiempo` tinyint(1) NOT NULL, `monedas` int(11) NOT NULL, `ranking` int(11) NOT NULL,
- `tiempoJugado` int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
