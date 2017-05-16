2c6acdb9dcb38be7b16a3ab6948532bd8613a98e
jnrubiano <jrubiaob@everis.com>
2017-05-16 15:25:49 -0500

    Se añade la entidad JugadorTienePersonaje: issue #479

---
+++ BACKEND/Polifight/src/java/entities/JugadorTienePersonaje.java
@@ -0,0 +1,82 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Entidad para la tabla: jugador_tiene_personaje
+ * @author jrubiaob
+ */
+@Entity
+@Table(name = "jugador_tiene_personaje")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "JugadorTienePersonaje.findAll", query = "SELECT j FROM JugadorTienePersonaje j"),
+    @NamedQuery(name = "JugadorTienePersonaje.findByIdJugadortienepersonaje", query = "SELECT j FROM JugadorTienePersonaje j WHERE j.idJugadortienepersonaje = :idJugadortienepersonaje")})
+public class JugadorTienePersonaje implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_Jugador_tiene_personaje")
+    private Integer idJugadortienepersonaje;
+    
+    @JoinColumn(name = "id_jugador", referencedColumnName = "id_jugador", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Jugador jugador;
+     
+    @JoinColumn(name = "id_personaje", referencedColumnName = "id_personaje", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Personaje personaje;
+    public JugadorTienePersonaje() {
+    }
+
+    public JugadorTienePersonaje(Integer idJugadortienepersonaje) {
+        this.idJugadortienepersonaje = idJugadortienepersonaje;
+    }
+
+    public Integer getIdJugadortienepersonaje() {
+        return idJugadortienepersonaje;
+    }
+
+    public void setIdJugadortienepersonaje(Integer idJugadortienepersonaje) {
+        this.idJugadortienepersonaje = idJugadortienepersonaje;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public Personaje getPersonaje() {
+        return personaje;
+    }
+
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
+    }  
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idJugadortienepersonaje != null ? idJugadortienepersonaje.hashCode() : 0);
+        return hash;
+    }
+    
+}
