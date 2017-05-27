972e12d22f06a21a85d90f174f388bb8c4c7f414
JuanSebastianRodriguezJ <sebastianrodriguez@MacBook-Pro-de-Sebastian-2.local>
2017-05-27 10:20:20 -0500

    resubido de pruebas psicotecnicas
    #534 Se hace una correccion en la implementacion de las pruebas
    psicotecnicas

--- FRONTEND/juego/js/pruebasPsicotecnicas/pruebasPsicotecnicas.js
+++ FRONTEND/juego/js/pruebasPsicotecnicas/pruebasPsicotecnicas.js
@@ -1,72 +1,53 @@
 //Son 22 acciones a realizar
-var prueba1 = null;
-var prueba2 = null;
-var prueba3 = null;
-var prueba4 = null;
-var prueba5 = null;
-var prueba6 = null;
-var prueba7 = null;
-var prueba8 = null;
-var prueba9 = null;
-var prueba10 = null;
-var prueba11 = null;
-var prueba12 = null;
-var prueba13 = null;
-var prueba14 = null;
-var prueba15 = null;
-var prueba16 = null;
-var prueba17 = null;
-var prueba18 = null;
-var prueba19 = null;
-var prueba20 = null;
-var prueba21 = null;
-var prueba22 = null;  
+var pregunta7 = null;
+var pregunta26 = null;
+var pregunta27 = null;
+var pregunta35 = null;
+var pregunta66 = null;
+var pregunta103 = null;
+var pregunta169 = null;
+var pregunta201 = null;
+var pregunta227 = null;
+var pregunta240 = null;
+var pregunta254 = null;
+var pregunta266 = null;
+var pregunta269 = null;
+var pregunta432 = null;
+var pregunta440 = null;
+var pregunta467 = null;
 
 //Cada variable tiene su Get y su Set
-var pruebasPsicotecnicas = {
-    getPrueba1: function() {return this.prueba1;},
-    getPrueba2: function() {return this.prueba2;},
-    getPrueba3: function() {return this.prueba3;},
-    getPrueba4: function() {return this.prueba4;},
-    getPrueba5: function() {return this.prueba5;},
-    getPrueba6: function() {return this.prueba6;},
-    getPrueba7: function() {return this.prueba7;},
-    getPrueba8: function() {return this.prueba8;},
-    getPrueba9: function() {return this.prueba9;},
-    getPrueba10: function() {return this.prueba10;},
-    getPrueba11: function() {return this.prueba11;},
-    getPrueba12: function() {return this.prueba12;},
-    getPrueba13: function() {return this.prueba13;},
-    getPrueba14: function() {return this.prueba14;},
-    getPrueba15: function() {return this.prueba15;},
-    getPrueba16: function() {return this.prueba16;},
-    getPrueba17: function() {return this.prueba17;},
-    getPrueba18: function() {return this.prueba18;},
-    getPrueba19: function() {return this.prueba19;},
-    getPrueba20: function() {return this.prueba20;},
-    getPrueba21: function() {return this.prueba21;},
-    getPrueba22: function() {return this.prueba22;},
+var preguntasPsicotecnicas = {
+    getpregunta7: function () {return this.pregunta1; },
+    getpregunta26: function() {return this.pregunta26; },
+    getpregunta27: function() {return this.pregunta27; },
+    getpregunta35: function() {return this.pregunta35; },
+    getpregunta66: function() {return this.pregunta66; },
+    getpregunta103: function() {return this.pregunta103; },
+    getpregunta169: function() {return this.pregunta169; },
+    getpregunta201: function() {return this.pregunta201; },
+    getpregunta227: function() {return this.pregunta227; },
+    getpregunta240: function() {return this.pregunta240; },
+    getpregunta254: function() {return this.pregunta254; },
+    getpregunta266: function() {return this.pregunta266; },
+    getpregunta269: function() {return this.pregunta269; },
+    getpregunta432: function() {return this.pregunta432; },
+    getpregunta440: function() {return this.pregunta440; },
+    getpregunta467: function() {return this.pregunta467; },
     
-    setPrueba1: function(x) {this.prueba1 = x;},
-    setPrueba2: function(x) {this.prueba2 = x;},
-    setPrueba3: function(x) {this.prueba3 = x;},
-    setPrueba4: function(x) {this.prueba4 = x;},
-    setPrueba5: function(x) {this.prueba5 = x;},
-    setPrueba6: function(x) {this.prueba6 = x;},
-    setPrueba7: function(x) {this.prueba7 = x;},
-    setPrueba8: function(x) {this.prueba8 = x;},
-    setPrueba9: function(x) {this.prueba9 = x;},
-    setPrueba10: function(x) {this.prueba10 = x;},
-    setPrueba11: function(x) {this.prueba11 = x;},
-    setPrueba12: function(x) {this.prueba12 = x;},
-    setPrueba13: function(x) {this.prueba13 = x;},
-    setPrueba14: function(x) {this.prueba14 = x;},
-    setPrueba15: function(x) {this.prueba15 = x;},
-    setPrueba16: function(x) {this.prueba16 = x;},
-    setPrueba17: function(x) {this.prueba17 = x;},
-    setPrueba18: function(x) {this.prueba18 = x;},
-    setPrueba19: function(x) {this.prueba19 = x;},
-    setPrueba20: function(x) {this.prueba20 = x;},
-    setPrueba21: function(x) {this.prueba21 = x;},
-    setPrueba22: function(x) {this.prueba22 = x;}
-}
+    setpregunta7: function(x) {this.pregunta7 = x;},
+    setpregunta26: function(x) {this.pregunta26 = x;},
+    setpregunta27: function(x) {this.pregunta27 = x;},
+    setpregunta35: function(x) {this.pregunta435 = x;},
+    setpregunta66: function(x) {this.pregunta66 = x;},
+    setpregunta103: function(x) {this.pregunta103 = x;},
+    setpregunta169: function(x) {this.pregunta169 = x;},
+    setpregunta201: function(x) {this.pregunta201 = x;},
+    setpregunta227: function(x) {this.pregunta227 = x;},
+    setpregunta240: function(x) {this.pregunta240 = x;},
+    setpregunta254: function(x) {this.pregunta254 = x;},
+    setpregunta266: function(x) {this.pregunta266 = x;},
+    setpregunta269: function(x) {this.pregunta269 = x;},
+    setpregunta432: function(x) {this.pregunta432 = x;},
+    setpregunta440: function(x) {this.pregunta440 = x;},
+    setpregunta467: function(x) {this.pregunta467 = x;},}
