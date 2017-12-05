be75f63bde74fa7f27143825629ad34da74126f5
Didiazhe <die.goo@hotmail.es>
2017-12-05 16:04:11 -0500

    Almacenamiento en el localstorage.

---
+++ FRONTEND/juego/js/localstorageB/localstorage.js
@@ -0,0 +1,6 @@
+//Funcion que permite almacenar cualquier valor en el localstorage recibiendo la llave y su valor
+function Persona(key, valor) {
+  console.log("prueba");
+  localStorage.setItem(key, valor);
+
+}
