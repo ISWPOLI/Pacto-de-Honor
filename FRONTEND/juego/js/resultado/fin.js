63eadb893cc2e17d55ac98a8247dd2d22bb69e9a
Didiazhe <die.goo@hotmail.es>
2017-12-06 09:11:12 -0500

    Guardado de datos al finalizar cada batalla

--- FRONTEND/juego/js/resultado/fin.js
+++ FRONTEND/juego/js/resultado/fin.js
@@ -2,20 +2,20 @@
 var fin = {
     preload: function(){
 		this.preloadBar=this.add.sprite(this.game.world.centerX,this.game.world.centerY,'barraCarga');
-		
+
         game.load.image('poli', '../img/componentes/finBatalla/poli.jpg');
-        game.load.image('monedas','../img/componentes/finBatalla/moneda.png');  
+        game.load.image('monedas','../img/componentes/finBatalla/moneda.png');
         game.load.image('experiencia','../img/componentes/finBatalla/experiencia.png');
         game.load.spritesheet('botonFinBatalla','../img/componentes/finBatalla/botonVolver.png',598,140);
-        
-        
+
+
     },
     create: function(){
 		var resultado;
 		var color;
         var exp;
         var coins;
-        
+
         /*console.log("Nombre: "+personajesBuenos[variablesCampoBatalla.idPJ].nombre);
         console.log("Daño normal: "+personajesBuenos[variablesCampoBatalla.idPJ].dano[0]);
         console.log("Daño especial: "+personajesBuenos[variablesCampoBatalla.idPJ].dano[1]);
@@ -24,8 +24,9 @@ var fin = {
         console.log("Energia: "+personajesBuenos[variablesCampoBatalla.idPJ].energia);
         console.log("Nivel: "+personajesBuenos[variablesCampoBatalla.idPJ].nivel);
         console.log("Experiencia: "+personajesBuenos[variablesCampoBatalla.idPJ].exp);*/
-       
+
 		if(variablesCampoBatalla.ganador){
+      var prueba =  variablesCampoBatalla.idNivel;
 			resultado = "GANASTE";
 			color = {fill:'#01DF01',font: '40px Arial'};
             coins = 200;
@@ -33,6 +34,8 @@ var fin = {
             exp = 100;
             datosperfil["datos"].experiencia += 100;
             personajesBuenos[variablesCampoBatalla.idPJ].exp += 100;
+            nivelMundoLocalStorage(prueba,0);
+
 		}else{
 			resultado = "PERDISTE";
 			color = {fill:'#DF0101',font: '40px Arial'};
@@ -40,10 +43,12 @@ var fin = {
             exp = 10;
             datosperfil["datos"].experiencia += 10;
             personajesBuenos[variablesCampoBatalla.idPJ].exp += 10;
+
+            nivelMundoLocalStorage(prueba,1);
 		}
         boot.verificarNivelJugador();
         boot.verificarNivelPersonajes();
-        
+
         /*console.log("Nombre: "+personajesBuenos[variablesCampoBatalla.idPJ].nombre);
         console.log("Daño normal: "+personajesBuenos[variablesCampoBatalla.idPJ].dano[0]);
         console.log("Daño especial: "+personajesBuenos[variablesCampoBatalla.idPJ].dano[1]);
@@ -52,70 +57,70 @@ var fin = {
         console.log("Energia: "+personajesBuenos[variablesCampoBatalla.idPJ].energia);
         console.log("Nivel: "+personajesBuenos[variablesCampoBatalla.idPJ].nivel);
         console.log("Experiencia: "+personajesBuenos[variablesCampoBatalla.idPJ].exp);*/
-        
+
 		if(variablesBoot.dispositivoMovil){
 			game.add.sprite(0, 0, 'poli');
         	personaje = game.add.sprite(game.world.width/2,game.world.height/1.3, 'personajeJugador');
         	personaje.scale.setTo(1.1);
         	personaje.anchor.setTo(1.1);
         	personaje.frame = 24;
-        
+
         	var name = datosperfil["datos"].nickname;
         	win= game.add.text(game.world.width/3,game.world.height/4,resultado,color);
         	win.anchor.x=0.5;
         	win.anchor.y = 0.5;
-        
+
         	game.add.text(game.world.width/5,game.world.height/1.25,name,color);
 
         	//se agrega el icono de las monedas ganadas por el jugador
         	monedas = game.add.sprite(game.world.width/1.7,game.world.height/2.5,'monedas');
         	monedas.scale.setTo(0.6);
         	monedas.anchor.setTo(0.4);
-        
+
         	//se agrega el boton de experiencia
         	experiencia = game.add.sprite(game.world.width/1.7,game.world.height/1.5,'experiencia');
         	experiencia.scale.setTo(0.6);
         	experiencia.anchor.setTo(0.4);
-        
+
         	//textos de la monedas y experiencia obtenida
         	game.add.text(game.world.width/1.4,game.world.height/2.78,'+ 250 ',{fill:'#FF8000',font: '40px Arial'});
-        
+
         	game.add.text(game.world.width/1.4,game.world.height/1.58,'+ 350',{fill:'#2E64FE',font: '40px Arial'});
-        
+
         	//se le agrega el boton que lo devuelve a la pantalla principal
         	volver = game.add.button(game.world.width/1.53,game.world.height/1.2,'botonFinBatalla',this.volver,1,1,0,2);
         	volver.scale.setTo(0.4);
         	volver.anchor.setTo(0.3);
-				
+
 		}else{
 			game.add.sprite(0, 0, 'poli');
         	personaje = game.add.sprite(game.world.width/2,game.world.height/1.3, 'personajeJugador');
         	personaje.scale.setTo(1.1);
         	personaje.anchor.setTo(1.1);
         	personaje.frame = 24;
-        
+
         	var name = datosperfil["datos"].nickname;
         	win= game.add.text(game.world.width/3,game.world.height/4,resultado,color);
         	win.anchor.x=0.5;
         	win.anchor.y = 0.5;
-        
+
         	game.add.text(game.world.width/5,game.world.height/1.25,name,color);
 
         	//se agrega el icono de las monedas ganadas por el jugador
         	monedas = game.add.sprite(game.world.width/1.7,game.world.height/2.5,'monedas');
         	monedas.scale.setTo(0.6);
         	monedas.anchor.setTo(0.4);
-        
+
         	//se agrega el boton de experiencia
         	experiencia = game.add.sprite(game.world.width/1.7,game.world.height/1.5,'experiencia');
         	experiencia.scale.setTo(0.6);
         	experiencia.anchor.setTo(0.4);
-        
+
         	//textos de la monedas y experiencia obtenida
         	game.add.text(game.world.width/1.4,game.world.height/2.78,'+ '+coins,{fill:'#FF8000',font: '40px Arial'});
-        
+
         	game.add.text(game.world.width/1.4,game.world.height/1.58,'+ '+exp,{fill:'#2E64FE',font: '40px Arial'});
-        
+
         	//se le agrega el boton que lo devuelve a la pantalla principal
         	volver = game.add.button(game.world.width/1.53,game.world.height/1.2,'botonFinBatalla',this.volver,1,1,0,2);
         	volver.scale.setTo(0.4);
