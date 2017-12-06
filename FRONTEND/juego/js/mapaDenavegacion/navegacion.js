13c65bf3cc87aa4a4079544ed08274713a8dd3fd
yamideus <wcubillosp@hotmail.com>
2017-12-06 18:43:07 -0500

    -Se añadio imagenes de los niveles bloqueados (niveles del 2-6) y mundos bloqueados -Validaciones para bloquear botones de niveles y botones de mundos referente al nivel y mundo mayores #5 #7

--- FRONTEND/juego/js/mapaDenavegacion/navegacion.js
+++ FRONTEND/juego/js/mapaDenavegacion/navegacion.js
@@ -27,7 +27,7 @@ vN = {// Variables encapsuladas del los botones y textos de los botones.
 
 };
 
-
+var mundoctual;
 var navegacion = function(game){};
     navegacion.prototype = {
     preload: function() {
@@ -56,12 +56,14 @@ var navegacion = function(game){};
         game.load.spritesheet('botonCompraPersonajes', '../img/componentes/navegacionMapa/botonCompraPersonajes.png', 62, 62);
         game.load.spritesheet('pause12', '../img/componentes/navegacionMapa/pause12.png', 50,50);
         game.load.spritesheet('pause13', '../img/componentes/navegacionMapa/pause13.png', 50,50);
+        game.load.spritesheet('pause13b', '../img/componentes/navegacionMapa/pause13b.png', 50,50);
         game.load.spritesheet('pause14', '../img/componentes/navegacionMapa/pause14.png', 50,50);
 
     },
 
     //se agrega el fondo y se crean los botones de los mundos en donde tenemos button(medida en x, medida en y, nombre de la imagen, la funcion, sprites)
     create:function() {
+        mundoctual = parseInt(obtenerLocalStorage('Mundo'));
         game.add.sprite(0, 0, 'fondo');//Se agrega un fondo.
         game.add.sprite(80, 10, 'monedas');//imagen monedas
         game.add.sprite(220, 10, 'xp').scale.setTo(0.4);//Imagen experiencia.
@@ -84,69 +86,161 @@ var navegacion = function(game){};
         game.add.text(160, 20,obtenerLocalStorage("Oro") , {font: "16px Roboto", fill: "#ffffff"}); //Label monedas desde perfilJugador
         game.add.text(275, 20, obtenerLocalStorage("Xp"), {font: "16px Roboto", fill: "#ffffff"}); //Label EXP desde perfilJugador
 
+
         vN.btMundo = game.add.button (80, 60, 'pause13', this.mundo1, 0, 0, 0, 1);
         vN.btMundo.scale.setTo(0.8, 0.8);
         vN.btMundo.input.useHandCursor = true;
         vN.text = game.add.text(13,1, "1", { font: "40px Arial", fill: "#030300"});
         vN.btMundo.addChild(vN.text);
 
-        vN.btMundo2 = game.add.button (90, 500, 'pause13',this.mundo2, 0, 0, 0, 1);
-        vN.btMundo2.scale.setTo(0.8, 0.8);
-        vN.btMundo.input.useHandCursor = true;
-        vN.text2 = game.add.text(13,1, "2", {font: "40px Arial", fill: "#030300"});
-        vN.btMundo2.addChild(vN.text2);
+        if(mundoctual<2){
+          vN.btMundo2 = game.add.button (90, 500, 'pause13b',this.mundo2, 0, 0, 0, 1);
+          vN.btMundo2.scale.setTo(0.8, 0.8);
+          vN.btMundo2.inputEnabled = false;
+          vN.btMundo.input.useHandCursor = false;
+          vN.text2 = game.add.text(13,1, "2", {font: "40px Arial", fill: "#030300"});
+          vN.btMundo2.addChild(vN.text2);
+        }
+          else{
+            vN.btMundo2 = game.add.button (90, 500, 'pause13',this.mundo2, 0, 0, 0, 1);
+            vN.btMundo2.scale.setTo(0.8, 0.8);
+            vN.btMundo.input.useHandCursor = true;
+            vN.text2 = game.add.text(13,1, "2", {font: "40px Arial", fill: "#030300"});
+            vN.btMundo2.addChild(vN.text2);
+          }
 
-        vN.btMundo3 = game.add.button (250, 450, 'pause13', this.mundo3, 0, 0, 0, 1);
+      if(mundoctual<3){
+        vN.btMundo3 = game.add.button (250, 450, 'pause13b', this.mundo3, 0, 0, 0, 1);
         vN.btMundo3.scale.setTo(0.8, 0.8);
+        vN.btMundo3.inputEnabled = false;
         vN.text3 = game.add.text(13,1, "3", { font: "40px Arial", fill: "#030300"});
         vN.btMundo3.addChild(vN.text3);
-
-        vN.btMundo4 = game.add.button (280, 220, 'pause13', this.mundo4, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo3 = game.add.button (250, 450, 'pause13', this.mundo3, 0, 0, 0, 1);
+          vN.btMundo3.scale.setTo(0.8, 0.8);
+          vN.text3 = game.add.text(13,1, "3", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo3.addChild(vN.text3);
+        }
+      if(mundoctual<4){
+        vN.btMundo4 = game.add.button (280, 220, 'pause13b', this.mundo4, 0, 0, 0, 1);
         vN.btMundo4.scale.setTo(0.8, 0.8);
+        vN.btMundo4.inputEnabled = false;
         vN.text4 = game.add.text(13,1, "4", { font: "40px Arial", fill: "#030300"});
         vN.btMundo4.addChild(vN.text4);
-
-        vN.btMundo5 = game.add.button (330, 80, 'pause13', this.mundo5, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo4 = game.add.button (280, 220, 'pause13', this.mundo4, 0, 0, 0, 1);
+          vN.btMundo4.scale.setTo(0.8, 0.8);
+          vN.text4 = game.add.text(13,1, "4", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo4.addChild(vN.text4);
+        }
+      if(mundoctual<5){
+        vN.btMundo5 = game.add.button (330, 80, 'pause13b', this.mundo5, 0, 0, 0, 1);
         vN.btMundo5.scale.setTo(0.8, 0.8);
+        vN.btMundo5.inputEnabled = false;
         vN.text5 = game.add.text(13,1, "5", { font: "40px Arial", fill: "#030300"});
         vN.btMundo5.addChild(vN.text5);
-
-        vN.btMundo6 = game.add.button (430, 500, 'pause13', this.mundo6, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo5 = game.add.button (330, 80, 'pause13', this.mundo5, 0, 0, 0, 1);
+          vN.btMundo5.scale.setTo(0.8, 0.8);
+          vN.text5 = game.add.text(13,1, "5", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo5.addChild(vN.text5);
+        }
+      if(mundoctual<6){
+        vN.btMundo6 = game.add.button (430, 500, 'pause13b', this.mundo6, 0, 0, 0, 1);
         vN.btMundo6.scale.setTo(0.8, 0.8);
+        vN.btMundo6.inputEnabled = false;
         vN.text6 = game.add.text(13,1, "6", { font: "40px Arial", fill: "#030300"});
         vN.btMundo6.addChild(vN.text6);
-
-        vN.btMundo7 = game.add.button (520, 220, 'pause13',this.mundo7, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo6 = game.add.button (430, 500, 'pause13', this.mundo6, 0, 0, 0, 1);
+          vN.btMundo6.scale.setTo(0.8, 0.8);
+          vN.text6 = game.add.text(13,1, "6", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo6.addChild(vN.text6);
+        }
+      if(mundoctual<7){
+        vN.btMundo7 = game.add.button (520, 220, 'pause13b',this.mundo7, 0, 0, 0, 1);
         vN.btMundo7.scale.setTo(0.8, 0.8);
+        vN.btMundo7.inputEnabled = false;
         vN.text7 = game.add.text(15,1, "7", { font: "40px Arial", fill: "#030300"});
         vN.btMundo7.addChild(vN.text7);
-
-        vN.btMundo8 = game.add.button (580, 200, 'pause13', this.mundo8, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo7 = game.add.button (520, 220, 'pause13',this.mundo7, 0, 0, 0, 1);
+          vN.btMundo7.scale.setTo(0.8, 0.8);
+          vN.text7 = game.add.text(15,1, "7", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo7.addChild(vN.text7);
+        }
+      if(mundoctual<8){
+        vN.btMundo8 = game.add.button (580, 200, 'pause13b', this.mundo8, 0, 0, 0, 1);
         vN.btMundo8.scale.setTo(0.8, 0.8);
+        vN.btMundo8.inputEnabled = false;
         vN.text8 = game.add.text(13,1, "8", { font: "40px Arial", fill: "#030300"});
         vN.btMundo8.addChild(vN.text8);
-
-        vN.btMundo9 = game.add.button (550, 80, 'pause13', this.mundo9, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo8 = game.add.button (580, 200, 'pause13', this.mundo8, 0, 0, 0, 1);
+          vN.btMundo8.scale.setTo(0.8, 0.8);
+          vN.text8 = game.add.text(13,1, "8", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo8.addChild(vN.text8);
+        }
+      if(mundoctual<9){
+        vN.btMundo9 = game.add.button (550, 80, 'pause13b', this.mundo9, 0, 0, 0, 1);
         vN.btMundo9.scale.setTo(0.8, 0.8);
+        vN.btMundo9.inputEnabled = false;
         vN.text9 = game.add.text(13,1, "9", { font: "40px Arial", fill: "#030300"});
         vN.btMundo9.addChild(vN.text9);
-
-        vN.btMundo10 = game.add.button (680, 140, 'pause13', this.mundo10, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo9 = game.add.button (550, 80, 'pause13', this.mundo9, 0, 0, 0, 1);
+          vN.btMundo9.scale.setTo(0.8, 0.8);
+          vN.text9 = game.add.text(13,1, "9", { font: "40px Arial", fill: "#030300"});
+          vN.btMundo9.addChild(vN.text9);
+        }
+      if(mundoctual<10){
+        vN.btMundo10 = game.add.button (680, 140, 'pause13b', this.mundo10, 0, 0, 0, 1);
         vN.btMundo10.scale.setTo(0.8, 0.8);
+        vN.btMundo10.inputEnabled = false;
         vN.text10 = game.add.text(6,3, "10", { font: "35px Arial", fill: "#030300"});
         vN.btMundo10.addChild(vN.text10);
-
-        vN.btMundo11 = game.add.button (700, 240, 'pause13', this.mundo11, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo10 = game.add.button (680, 140, 'pause13', this.mundo10, 0, 0, 0, 1);
+          vN.btMundo10.scale.setTo(0.8, 0.8);
+          vN.text10 = game.add.text(6,3, "10", { font: "35px Arial", fill: "#030300"});
+          vN.btMundo10.addChild(vN.text10);
+        }
+      if(mundoctual<11){
+        vN.btMundo11 = game.add.button (700, 240, 'pause13b', this.mundo11, 0, 0, 0, 1);
         vN.btMundo11.scale.setTo(0.8, 0.8);
+        vN.btMundo11.inputEnabled = false;
         vN.text11 = game.add.text(9,3, "11", { font: "35px Arial", fill: "#030300"});
         vN.btMundo11.addChild(vN.text11);
-
-        vN.btMundo12 = game.add.button (750, 480, 'pause14',this.mundo12, 0, 0, 0, 1);
+      }
+        else{
+          vN.btMundo11 = game.add.button (700, 240, 'pause13', this.mundo11, 0, 0, 0, 1);
+          vN.btMundo11.scale.setTo(0.8, 0.8);
+          vN.text11 = game.add.text(9,3, "11", { font: "35px Arial", fill: "#030300"});
+          vN.btMundo11.addChild(vN.text11);
+        }
+      if(mundoctual<12){
+        vN.btMundo12 = game.add.button (750, 480, 'pause13b',this.mundo12, 0, 0, 0, 1);
         vN.btMundo12.scale.setTo(0.8, 0.8);
+        vN.btMundo12.inputEnabled = false;
         vN.text12 = game.add.text(5,1, "12", { font: "35px Arial", fill: "#030300"});
         vN.btMundo12.addChild(vN.text12);
-
+      }
+        else{
+          vN.btMundo12 = game.add.button (750, 480, 'pause14',this.mundo12, 0, 0, 0, 1);
+          vN.btMundo12.scale.setTo(0.8, 0.8);
+          vN.text12 = game.add.text(5,1, "12", { font: "35px Arial", fill: "#030300"});
+          vN.btMundo12.addChild(vN.text12);
+        }
         boot.verificarMusica("mapa");
+        cargaInicial();
     },
 
     verAjustes: function(){
