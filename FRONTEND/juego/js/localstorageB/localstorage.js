68c433c39f4d2476afe6c17c66b79b4df0e1bbdc
Didiazhe <die.goo@hotmail.es>
2017-12-06 09:05:46 -0500

    Modificacion metodos

--- FRONTEND/juego/js/localstorageB/localstorage.js
+++ FRONTEND/juego/js/localstorageB/localstorage.js
@@ -1,6 +1,47 @@
+
+
 //Funcion que permite almacenar cualquier valor en el localstorage recibiendo la llave y su valor
-function Persona(key, valor) {
-  console.log("prueba");
+function añadirLocalStorage(key, valor) {
+
   localStorage.setItem(key, valor);
 
+}
+//funcion encargada de dar al localStorage el mundo y nivel actual si gana
+function nivelMundoLocalStorage(nivelo,validacion){
+  if(validacion==0){
+    var orotemp=parseInt(localStorage.getItem("Oro"));
+    var xptemp=parseInt(localStorage.getItem("Xp"));
+    var mundo;
+    var nivel;
+    for (var i = 0; i < nivelo.length; i++) {
+      var caracter = nivelo.charAt(i);
+  //obtenemos el nivel
+      if(caracter=="-"){
+        for (var x= i+1; x < nivelo.length; x++) {
+        nivel=+nivelo.charAt(x);
+      }
+      break;
+
+    }
+      else{
+      //obtenemoselmundo
+        mundo=+nivelo.charAt(i);
+        }
+  }
+  console.log(nivelo);
+  console.log("NIVEL MUNDO " +nivel);
+  console.log("MUNDO " +mundo);
+    orotemp+=200;
+    xptemp+=100;
+
+
+    añadirLocalStorage("Oro",orotemp);
+    añadirLocalStorage("Xp",xptemp);
+    añadirLocalStorage("NivelMundo",nivel);
+    añadirLocalStorage("Mundo",mundo);
+  }
+  else {
+    xptemp+=10;
+    añadirLocalStorage("Xp",xptemp);
+  }
 }
